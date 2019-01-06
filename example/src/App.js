9738b05a22e0397b98c08632a97a334814b398bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-06 19:16:24 +0000

    Deploy thesmartwon/react-test-lib-9000 to github.com/thesmartwon/react-test-lib-9000.git:gh-pages

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-test-lib-9000'
+import ExampleComponent from 'react-test-lib'
 
 export default class App extends Component {
   render () {
